6670619f4023c15aa0eadff603ddc63845137dcb
Auric Vente <mprops@protonmail.com>
2026-03-07 12:36:56 -0600

    Mods

--- utils.py
+++ utils.py
@@ -5,6 +5,19 @@ import random
 
 
 class Utils:
+    def __init__(self):
+        self.ansi_colors = {
+            "black": "\033[30m",
+            "red": "\033[31m",
+            "green": "\033[32m",
+            "yellow": "\033[33m",
+            "blue": "\033[34m",
+            "magenta": "\033[35m",
+            "cyan": "\033[36m",
+            "white": "\033[37m",
+            "reset": "\033[0m",
+        }
+
     def get_random_name(self):
         dict_path = "/usr/share/dict/words"
 
@@ -36,28 +49,16 @@ class Utils:
 
         return False
 
-    ANSI_COLORS = {
-        "black": "\033[30m",
-        "red": "\033[31m",
-        "green": "\033[32m",
-        "yellow": "\033[33m",
-        "blue": "\033[34m",
-        "magenta": "\033[35m",
-        "cyan": "\033[36m",
-        "white": "\033[37m",
-        "reset": "\033[0m"
-    }
-
-    def print(self, text, color = ""):
+    def print(self, text, color=""):
         if color:
             color_key = color.lower()
 
-            if color_key in ANSI_COLORS:
-                color_code = ANSI_COLORS[color_key]
+            if color_key in self.ansi_colors:
+                color_code = self.ansi_colors[color_key]
             else:
                 color_code = ""
 
-            print(f"{color_code}{text}{ANSI_COLORS['reset']}")
+            print(f"{color_code}{text}{self.ansi_colors['reset']}")
         else:
             print(text)
 
