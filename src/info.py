80d6ed5dca7b5ecb6f385e20e3134de69c3c9beb
Auric Vente <mprops@protonmail.com>
2026-03-07 16:39:35 -0600

    Mods

--- src/info.py
+++ src/info.py
@@ -1,17 +1,16 @@
-import tomllib
-from pathlib import Path
+from setuptools import setup
 
-class Info:
-    name: str
-    version: str
+from src.info import info
 
-    def __init__(self) -> None:
-        toml_path = Path(__file__).parent / "info.toml"
-
-        with open(toml_path, "rb") as f:
-            info_dict = tomllib.load(f)
-
-        for key, value in info_dict.items():
-            setattr(self, key, value)
-
-info = Info()
+setup(
+    name=info.name,
+    version=info.version,
+    package_dir={"": "src"},
+    packages=[""],
+    package_data={"": ["*.toml"]},
+    entry_points={
+        "console_scripts": [
+            "hugegull = main:main",
+        ],
+    },
+)
